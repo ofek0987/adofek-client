191417f7008f04e00ff9a4f47022b2b51dd25304
Your Name <you@example.com>
2022-06-05 17:23:59 +0300

    Fix application_logic syntax add documentation.

--- application_logic.py
+++ application_logic.py
@@ -1,8 +1,10 @@
-from users import my_user
-import contacts_collection
+from users.user import IUser
+from contacts_collection import ContactsCollection
 
 
 class ApplicationLogic:
+    """Handle events from gui class."""
 
-    my_user: my_user
-    contacts: contacts_collection
+    def __init__(self, my_user: IUser, contacts: ContactsCollection):
+        self.my_user = my_user
+        self.contacts = contacts
