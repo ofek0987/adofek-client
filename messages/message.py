9387fa6e6a7d36bbc7f3c969702529ff944ffadb
Your Name <you@example.com>
2022-06-05 16:52:07 +0300

    Fix IMessage syntax and add doc

--- messages/message.py
+++ messages/message.py
@@ -1,9 +1,14 @@
+from abc import ABC, abstractmethod
+
 import tkinter
 
 
-class IMessage:
+class IMessage(ABC):
+    """Interface representing all the messages that can be sent/received, for example a text message"""
+    def __init__(self, identifier: str):
+        self.identifier = identifier
 
-    id: str
-
+    @abstractmethod
     def display(self) -> tkinter.Label:
+        """Will be used by the GUI in order to display the message"""
         pass
