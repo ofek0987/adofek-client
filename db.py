9de44f3be7327aa99fe370f1138e727983e9171c
Your Name <you@example.com>
2022-06-05 17:35:07 +0300

    Fix DB syntax and adding documentation

--- db.py
+++ db.py
@@ -1,17 +1,23 @@
-from users import other_user
-from messages import message
+from users.other_user import OtherUser
+from messages.message import IMessage
+from users.user import IUser
+from contacts_collection import ContactsCollection
 
 
 class DB:
-
-    #def get_contacts(self): list[OtherUser] # what is the currect syntax ?
-     #   pass
-
-    def get_messages_history_by_user(user: other_user) -> list[message]:
+    """Database management for client."""
+    def get_contacts(self) -> ContactsCollection:
         pass
 
-    def add_contact(user: other_user):
+    def get_messages_history_by_user(self, user: IUser) -> list[IMessage]:
+        """
+        Return message history filtered by a given user.
+        :param user: The user to filter the messages by.
+        """
         pass
 
-    def add_message(IMessage):
+    def add_contact(self, user: OtherUser):
+        pass
+
+    def add_message(self, message: IMessage):
         pass
