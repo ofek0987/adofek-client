38853c0d450fc254cdeed094db20b4e00486e88a
Your Name <you@example.com>
2022-06-05 17:03:57 +0300

    Fix OtherUser syntax and add docs

--- users/other_user.py
+++ users/other_user.py
@@ -1,10 +1,16 @@
-import db
-from messages import message
+from dataclasses import dataclass
+
+from db import DB
+from messages.message import IMessage
+from users.user import IUser
 
 
-class OtherUser:
+@dataclass
+class OtherUser(IUser):
+    """A user that is not the user that is used by the local user."""
+    db: DB
 
-    db:db
-
-    def get_messages_history(self) -> list[message]:
+    def get_messages_history(self) -> list[IMessage]:
+        """Returns all the messages history stored in the database,
+         sorted by their's timestamp in descending order."""
         pass
