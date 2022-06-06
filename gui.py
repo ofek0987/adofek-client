4c4dd74a78462f7a3dd4c258fe72bc832424b4bb
adi <adim333@gmail.com>
2022-06-06 06:50:01 +0300

    Adding two methods we can consider how we want to manage the gui states and figure out what method will we use (display(target,state) or (display_sign_in,display_logged_form,...) let me know.

--- gui.py
+++ gui.py
@@ -2,8 +2,19 @@ import tkinter
 
 
 class GUI:
-    """Manage all the gui."""
+    """Manage all the gui, ***note use display method and render each state by param given
+        or consider making own methods for any state."""
 
+    def display(self,target,state):  # optimal
+        """
+            Display the current form from state stack. (optimal)
+        :param target: Target windows to render.
+        :param state: State form to render (sign_in_state,logged_state,...).
+        """
+
+    def display_sign_in_state(self):  # optimal
+        """Use LoginWindow class to create sign in form (optimal)"""
+        pass
 
     @staticmethod
     def create_text_message_label(text) -> tkinter.Label:
