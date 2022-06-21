7b78fffd42c941bd78f254556cd72ae04311d067
adi <adim333@gmail.com>
2022-06-22 02:10:44 +0300

    login_state.py update syntax to PEP8 and fix bugs implement login_state.py inside gui class

--- gui.py
+++ gui.py
@@ -1,20 +1,39 @@
 import tkinter
+from login_state import LoginWindow
 
 
 class GUI:
     """Manage all the gui, ***note use display method and render each state by param given
         or consider making own methods for any state."""
 
-    def display(self,target,state):  # optimal
-        """
-            Display the current form from state stack. (optimal)
-        :param target: Target windows to render.
-        :param state: State form to render (sign_in_state,logged_state,...).
-        """
+    def __init__(self):
+        self.main_window = None
+        self.login_state = None
 
-    def display_sign_in_state(self):  # optimal
-        """Use LoginWindow class to create sign in form (optimal)"""
+    def display_splash_screen(self):
         pass
+
+    def display_sign_in_state(self):
+        """Use LoginWindow class to create sign in form"""
+        self.main_window = tkinter.Tk()
+        self.login_state = LoginWindow(self.main_window)
+        self.login_state.create_frame_layer()
+        self.login_state.create_sign_in_layer()
+        self.login_state.create_logo()
+        self.login_state.create_login_button()
+        self.login_state.create_forget_password_button()
+        self.login_state.create_password_input()
+        self.login_state.create_password_logo()
+        self.login_state.create_password_show_or_hide_button()
+        self.login_state.create_username_input()
+        self.login_state.create_username_logo()
+
+    def update(self):
+        self.main_window.update()
+        self.main_window.mainloop()
+
+    def clear(self):
+        self.main_window.destroy()
 
     @staticmethod
     def create_text_message_label(text) -> tkinter.Label:
@@ -27,3 +46,4 @@ class GUI:
     @staticmethod
     def create_file_message_label(text) -> tkinter.Label:
         pass
+
